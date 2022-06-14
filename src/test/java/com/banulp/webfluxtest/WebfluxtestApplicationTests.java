1fed50b8d93b8156cbd6a4ecfc8af2833c6aad13
박경옥(Park-KyungOk)(banulp) <banulp@ncsoft.com>
2022-06-14 18:42:43 +0900

    aa

---
+++ src/test/java/com/banulp/webfluxtest/WebfluxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banulp.webfluxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
