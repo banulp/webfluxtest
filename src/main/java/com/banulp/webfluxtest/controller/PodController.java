78d3db19b8505a740b5110452b8ddd08d926845c
박경옥(Park-KyungOk)(banulp) <banulp@ncsoft.com>
2022-06-14 19:44:06 +0900

    link 여러개로 바꾸고 응답값이랑 묶어서 내보내기

--- src/main/java/com/banulp/webfluxtest/controller/PodController.java
+++ src/main/java/com/banulp/webfluxtest/controller/PodController.java
@@ -1,22 +1,12 @@
-package com.ncsoft.gamedata.gdconfig.admin.controller;
+package com.banulp.webfluxtest.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.MediaType;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 
 @Slf4j
 @RestController
@@ -24,43 +14,18 @@ import java.util.stream.Collectors;
 public class PodController {
 
     @GetMapping("/envs")
-    public Flux<String> application(@PathVariable("application") String application){
+    public Flux<String> application(){
             WebClient client = WebClient.create();
 
-            List<String> tl = Arrays.asList("http://172.19.66.127:30632", "http://172.19.66.127:30632");
+            List<String> tl = Arrays.asList("https://jsonplaceholder.typicode.com/posts/1");
             return Flux.just(tl.toArray())
-//            return Flux.just(podCollect.toArray())
-                    .flatMap(pod -> {
-                        log.info("pod env call");
-//                        return client.post()
+                    .flatMap(link -> {
                         return client.get()
-                                .uri(pod + "/monitor/env")
+                                .uri((String) link)
 //                                .contentType(MediaType.APPLICATION_JSON)
                                 .retrieve()
                                 .bodyToFlux(String.class);
-//                                .map(s -> {
-//                                    return s.trim();
-//                                });
                     });
-        return null;
-    }
-
-    @PostMapping
-    public Flux<String> refresh(@RequestBody List<String> podList) {
-        WebClient client = WebClient.create();
-//        log.info("pod refresh start only 12");
-
-//        List<String> tl = Arrays.asList("http://172.19.66.127:30632", "http://172.19.66.127:30632");
-//        return Flux.just(tl.toArray())
-                return Flux.just(podList.toArray())
-                .flatMap(pod -> {
-                    log.info("pod refresh start");
-                    return client.post()
-                            .uri(pod + "/monitor/refresh")
-                            .contentType(MediaType.APPLICATION_JSON)
-                            .retrieve()
-                            .bodyToMono(String.class);
-                });
     }
 
 }
